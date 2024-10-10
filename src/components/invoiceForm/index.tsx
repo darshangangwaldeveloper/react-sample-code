cba45582c4f82827788f6d610426d39e13fd96aa
DARSHAN <darshangangwaldeveloper@gmail.com>
2024-10-10 21:48:34 +0530

    changes in index file

--- src/components/invoiceForm/index.tsx
+++ src/components/invoiceForm/index.tsx
@@ -171,9 +171,7 @@ console.log("object")
 console.log("object")
 console.log("object")
 
-console.log("object")
-console.log("object")
-console.log("object")
+
 
   const handleSaveAsDraft = async () => {
     const payload = transformPayload(formik.values);
